bd65781af3597d07bbc5120fd99718c5358af828
sifatkhaan <sifatkhan9330@gmail.com>
2024-11-14 17:29:02 +0600

    actions fixed

--- app/component/test/Todo.tsx
+++ app/component/test/Todo.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import {ACTIONS} from '../../practice/page'
-export default function Todo({todo, dispatch}) {
+
+import React from 'react';
+import { ACTIONS } from '../../practice/page';
+
+interface TodoType {
+  id: number;
+  name: string;
+  complete: boolean;
+}
+
+interface TodoProps {
+  todo: TodoType;
+  dispatch: React.Dispatch<{ type: string, payload: any }>;
+}
+
+export default function Todo({ todo, dispatch }: TodoProps) {
   return (
     <div>
-        <span style={{color:todo.complete ? '#AAA' : '#000'}}>{todo.name}</span>
-        <button className='px-10' onClick={()=> dispatch({type: ACTIONS.TOGGLE_TODO, payload:{id:todo.id}})}>Toggle</button>
-        <button onClick={()=> dispatch({type: ACTIONS.DELETE_TODO, payload:{id:todo.id}})}>Delete</button>
+      <span style={{ color: todo.complete ? '#AAA' : '#000' }}>{todo.name}</span>
+      <button className="px-10" onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
     </div>
-  )
+  );
 }
