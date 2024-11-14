abcd3d2cbbd26c0dfec0796ca3cbca7ca05182f8
sifatkhaan <sifatkhan9330@gmail.com>
2024-11-14 18:16:53 +0600

    token added

--- app/component/test/Todo.tsx
+++ app/component/test/Todo.tsx
@@ -17,8 +17,8 @@ export default function Todo({ todo, dispatch }: TodoProps) {
   return (
     <div>
       <span style={{ color: todo.complete ? '#AAA' : '#000' }}>{todo.name}</span>
-      <button className="px-10" onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
-      <button onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
+      <button className="px-10 bg-gray-400" onClick={() => dispatch({ type: ACTIONS.TOGGLE_TODO, payload: { id: todo.id } })}>Toggle</button>
+      <button className='bg-gray-400' onClick={() => dispatch({ type: ACTIONS.DELETE_TODO, payload: { id: todo.id } })}>Delete</button>
     </div>
   );
 }
